df7e3f61a7e237560d4a448639a336007fd39929
Ahmet Selim Boz <ahmetselimboz46@gmail.com>
2024-08-19 13:55:51 +0300

    00

--- client/src/index.js
+++ client/src/index.js
@@ -4,10 +4,16 @@ import "./index.css";
 import App from "./App";
 import Footer from "./components/footer.tsx"
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+
+
+
 const client = new ApolloClient({
   uri: process.env.REACT_APP_GRAPHQL_API_URL,
   cache: new InMemoryCache(),
 });
+
+console.log(process.env.REACT_APP_GRAPHQL_API_URL)
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
